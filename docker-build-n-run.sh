aa0f51c71f016db523964e816fbaf0896e1396cc
Jérôme Breton <jerome.breton@gmail.com>
2017-07-18 15:22:52 +0200

    Change container name

--- docker-build-n-run.sh
+++ docker-build-n-run.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname "$0")
 
-docker build -t jeromebreton/xhprof-simple-gui:source .
-docker run --rm -p 3731:80 --name jeromebreton-xhprof-simple-gui -v "`pwd`/traces":/traces jeromebreton/xhprof-simple-gui:source
+docker build -t jeromebreton/xhprof-simple-viewer:source .
+docker run --rm -p 3731:80 --name jeromebreton-xhprof-simple-viewer -v "`pwd`/traces":/traces jeromebreton/xhprof-simple-viewer:source
 
 cd -
